0589d3c6c9422eadf9588235c6921a0828826abc
JoeyGordon <joey.gordon@gmail.com>
2019-07-15 21:58:31 -0700

    use postgres

--- src/server.js
+++ src/server.js
@@ -2,12 +2,15 @@ const express = require("express");
 const peopleRoutes = require("./views/v1/people");
 const statusRoutes = require("./views/status");
 
-const PORT = 6666;
+const PORT = 1234;
 
 const app = express();
-app.use("/", statusRoutes);
-app.use("/v1/", peopleRoutes);
 
 app.listen(process.env.PORT || PORT, () =>
   console.log(`Server listening on port ${process.env.PORT || PORT}!`)
 );
+
+app.use(express.json());
+
+app.use("/", statusRoutes);
+app.use("/v1/", peopleRoutes);
--- src/views/v1/people.js
+++ src/views/v1/people.js
@@ -1,31 +1,44 @@
 const express = require("express");
 const peopleData = require("../../data/people");
+const models  = require('../../models');
 
 const router = express.Router();
 
 router.get("/people", (req, res) => {
-  res.status(200).send({
-    success: "true",
-    message: "People retrieved successfully",
-    people: peopleData
+  models.Person.findAll()
+    .then(function(people) {
+      res.status(200).send({
+        success: "true",
+        message: "People retrieved successfully",
+        people: people
+      });
+  });
+});
+
+router.post("/person", (req, res) => {
+  console.log(req.body);
+  models.Person.create({
+    name: req.body.name,
+    description: req.body.description,
+  })
+  .then(person => {
+    res.status(200).send({
+      success: 'true',
+      message: 'successfully created person',
+      person
+    });
   });
 });
 
 
 router.get("/person/:id", (req, res) => {
-  const id = parseInt(req.params.id, 10);
-  peopleData.map(person => {
-    if (person.id === id) {
-      return res.status(200).send({
-        success: "true",
-        message: "Person retrieved successfully",
-        person: [person]
-      });
-    }
-  });
-  return res.status(404).send({
-    success: "false",
-    message: "person does not exist"
+  // const id = parseInt(req.params.id, 10);
+  models.Person.findByPk(req.params.id).then(person => {
+    res.status(200).send({
+      success: "true",
+      message: "Person retrieved successfully",
+      person
+    });
   });
 });
 
