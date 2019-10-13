cf58bbdaf581dbfa814596c51de417738718076a
Joey Gordon <joey.gordon@gmail.com>
2019-10-13 11:00:54 -0700

    add routes

--- src/server.js
+++ src/server.js
@@ -2,15 +2,17 @@ const express = require('express');
 const peopleRoutes = require('./views/v1/people');
 const statusRoutes = require('./views/status');
 const timersRoutes = require('./views/v1/timers');
+const jobsRoutes = require('./views/v1/jobs');
 
 const PORT = 1234;
 
 const app = express();
 
-app.listen(process.env.PORT || PORT, () => console.log(`Server listening on port ${process.env.PORT || PORT}!`));
+app.listen(process.env.PORT || PORT, () => console.log(`Server listening on port ${process.env.PORT || PORT}!`),);
 
 app.use(express.json());
 
 app.use('/', statusRoutes);
+app.use('/v1/', jobsRoutes);
 app.use('/v1/', peopleRoutes);
 app.use('/v1/', timersRoutes);
